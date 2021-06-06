936d80ee77725ec371f3508601c2d3cd43556fa7
Tejas9955 <79703652+Tejas9955@users.noreply.github.com>
2021-06-06 12:48:07 +0530

    Update Security.js

--- js/Security.js
+++ js/Security.js
@@ -2,7 +2,7 @@ class Security {
 
     constructor(){
 
-        this.access1 = createInput("Answer")
+        this.access1 = createInput("Code")
         this.access1.position(100,90);
         this.access1.style('background', 'white');  
 
@@ -10,7 +10,7 @@ class Security {
         this.button1.position(100,120);
         this.button1.style('background', 'lightgrey');    
 
-        this.access2 = createInput("Answer")
+        this.access2 = createInput("Code")
         this.access2.position(700,190);
         this.access2.style('background', 'white');  
 
@@ -18,7 +18,7 @@ class Security {
         this.button2.position(700,220);
         this.button2.style('background', 'lightgrey');
 
-        this.access3 = createInput("Answer")
+        this.access3 = createInput("Code")
         this.access3.position(100,290);
         this.access3.style('background', 'white');  
 
